d445f4365f302c777e762e39080853c70fe955d1
Adam Starke <afstarke@gmail.com>
2016-01-22 08:41:17 -0500

    initial migration to web-loaded data

--- staticPlots.R
+++ staticPlots.R
@@ -60,33 +60,6 @@ static_plot3  %>% dyShading(from = 2.3, to = -1, axis = "y", color = "#ffd1d1") 
 
 
 
-## Cumulative Plots:
-
-
-DO_CumulativePlot <- function(data, site){
-	cumdata <- data %>% 
-		rename(DO_0.5m = X_0.5m.above.seabed_00300_00011) %>% 
-		select(DO_0.5m) %>% 
-		filter(complete.cases(.)) %>% 
-		mutate(cumDistDO_0.5m = cume_dist(DO_0.5m)*100) %>% 
-		mutate(O2_criteria = ifelse(DO_0.5m <= 2.3, 
-					    "Survival in jeopordy", 
-					    ifelse(DO_0.5m >= 4.8, "No harm", "Growth hindered"))) %>% 
-		mutate(O2_criteria = factor(O2_criteria, levels = c("Survival in jeopordy", "Growth hindered", "No harm"))) %>% 
-		ggplot(aes(y = DO_0.5m, x = cumDistDO_0.5m))+
-		geom_ribbon(aes(ymin = 0, ymax = DO_0.5m, fill = O2_criteria)) +
-		theme_bw() + 
-		scale_fill_manual(values = c('#FFB4A2', '#FCDDBC', '#B8D8BA')) +
-		labs(x = "% of total time", y = "DO concentration (mg/L)", 
-		     fill = "Effects of O2 concentration", title = paste("Impacts of Oxygen Limitations -", site, sep = " ")) 
-	cumdata
-}
-
-DO_CumulativePlot(site1_Rawdata, "Site 1")
-DO_CumulativePlot(site2_Rawdata, "Site 2")
-DO_CumulativePlot(site3_Rawdata, "Site 3")
-
-
 
 
 
